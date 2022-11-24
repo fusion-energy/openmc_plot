2f5604040e65677e04caebf27eb4d1b6738efdf2
shimwell <mail@jshimwell.com>
2022-11-24 23:36:44 +0000

    corrected basic geom file

--- examples/tokamak/create_geometry_xml_files.py
+++ examples/tokamak/create_geometry_xml_files.py
@@ -41,3 +41,35 @@ universe = openmc.Universe(cells=[central_column_cell, firstwall_cell,
 my_geometry = openmc.Geometry(universe)
 
 my_geometry.export_to_xml('geometry_some_materials.xml')
+
+
+mat_1 = openmc.Material()
+mat_1.id = 1
+mat_1.add_element('Na', 1)
+mat_1.set_density('g/cm3', 1)
+central_column_cell.fill = mat_1
+
+mat_2 = openmc.Material()
+mat_2.id = 2
+mat_2.add_element('Na', 1)
+mat_2.set_density('g/cm3', 1)
+firstwall_cell.fill = mat_2
+
+mat_3 = openmc.Material()
+mat_3.id = 2
+mat_3.add_element('Na', 1)
+mat_3.set_density('g/cm3', 1)
+blanket_cell.fill = mat_3
+
+mat_4 = openmc.Material()
+mat_4.id = 2
+mat_4.add_element('Na', 1)
+mat_4.set_density('g/cm3', 1)
+inner_vessel_cell.fill = mat_4
+
+universe = openmc.Universe(cells=[central_column_cell, firstwall_cell,
+                                  blanket_cell, inner_vessel_cell])
+
+my_geometry = openmc.Geometry(universe)
+
+my_geometry.export_to_xml('geometry.xml')
