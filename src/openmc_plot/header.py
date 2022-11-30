46bc6a42c325a7d831a4948e7a18839039d66356
Jonathan Shimwell <mail@jshimwell.com>
2022-11-30 11:44:59 +0000

    bump vversion

--- src/openmc_plot/header.py
+++ src/openmc_plot/header.py
@@ -22,7 +22,7 @@ def header():
 
     st.write(
         """
-            # OpenMC plot ```v0.1.11```
+            # OpenMC plot ```v0.1.13```
             ### ⚛ A geometry plotting user interface for OpenMC.
 
             🐍 Run this app locally with Python ```pip install openmc_plot``` then run with ```openmc_plot```
