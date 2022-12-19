1fccb2b7565362b04e0cdd26931d9eee37e802a4
Jonathan Shimwell <mail@jshimwell.com>
2022-12-19 15:17:39 +0000

    corrected example ww link

--- src/openmc_plot/weightwindows_tab.py
+++ src/openmc_plot/weightwindows_tab.py
@@ -22,7 +22,7 @@ def create_weightwindow_tab():
         st.markdown(new_title, unsafe_allow_html=True)
 
         st.markdown(
-            'Not got xml files handy? Download sample [settings.xml](https://raw.githubusercontent.com/fusion-energy/openmc_plot/adding_ww_plotter/examples/weightwindows/settings.xml)'
+            'Not got xml files handy? Download sample [settings.xml](https://raw.githubusercontent.com/fusion-energy/openmc_plot/main/examples/weightwindows/settings.xml)'
         )
 
     else:
@@ -30,7 +30,7 @@ def create_weightwindow_tab():
         save_uploadedfile(settings_xml_file)
 
         my_settings = openmc.Settings.from_xml(settings_xml_file.name)
-        
+
         weight_windows = my_settings.weight_windows
 
         print('weight_windows', weight_windows)
@@ -65,83 +65,89 @@ def create_weightwindow_tab():
 
             mesh = selected_weight_window.mesh
 
-            reshaped_tally = plotted_part_of_weight_window.reshape(mesh.dimension, order="F")
+            if isinstance(mesh, openmc.CylindricalMesh):
+                st.write('weight windows on CylindricalMesh are not supported')
+            elif isinstance(mesh, openmc.RegularMesh):
 
-            axis_to_slice = col1.selectbox(
-                label="Slice plane", options=("X", "Y", "Z"), index=0, key='ww_axis_to_slice'
-            )
+                reshaped_tally = plotted_part_of_weight_window.reshape(mesh.dimension, order="F")
 
-            if axis_to_slice == "X":
-                tally_aligned = reshaped_tally.transpose(1, 2, 0)
-                bb_index = 0
-                x_label = "Y [cm]"
-                y_label = "Z [cm]"
-            elif axis_to_slice == "Y":
-                tally_aligned = reshaped_tally.transpose(
-                    0, 1, 2
+                axis_to_slice = col1.selectbox(
+                    label="Slice plane", options=("X", "Y", "Z"), index=0, key='ww_axis_to_slice'
                 )
-                bb_index = 1
-                x_label = "X [cm]"
-                y_label = "Z [cm]"
-            else:  # axis_to_slice == 'Z':
-                tally_aligned = reshaped_tally.transpose(2, 0, 1)
-                bb_index = 2
-                x_label = "X [cm]"
-                y_label = "Y [cm]"
 
-            left = mesh.lower_left[bb_index]
-            right = mesh.upper_right[bb_index]
-            bottom = mesh.lower_left[bb_index]
-            top = mesh.upper_right[bb_index]
-
-            slice_value = col1.slider(
-                label="slice index",
-                min_value=0,
-                max_value=len(tally_aligned) - 1,
-                value=int(len(tally_aligned) / 2),
-            )
-
-            log_lin_scale = col1.radio(
-                "Normalization", options=["log", "linear"],
-                key='ww_log_lin_scale')
-            if log_lin_scale == "linear":
-                norm = None
-            else:
-                norm = LogNorm()
-
-            image_slice = tally_aligned[slice_value]
-
-            if axis_to_slice == "Y":
-                image_slice = np.rot90(image_slice)
-            if axis_to_slice == "Z":
-                image_slice = np.rot90(image_slice)
-            if axis_to_slice == "X":
-                image_slice = np.flipud(image_slice)
-
-            plt.cla()
-            plt.clf()
-
-            plt.axes(title="Tally value", xlabel=x_label, ylabel=y_label)
-            # could be assigned like this
-            # plt.xlabel(x_label)
-            # plt.ylabel(y_label)
-            # plt.title('Tally value')
-            if np.amax(image_slice) == np.amin(image_slice) and norm is not None:
-                msg = "slice contains the uniform values, can't be plotted on log scale"
-                format = f'<p style="font-family:sans-serif; color:Red; font-size: 30px;">{msg}</p>'
-                col2.markdown(format, unsafe_allow_html=True)
-            else:
-                plt.imshow(X=image_slice,
-                    extent=(left, right, bottom, top), norm=norm
-                )
-                plt.colorbar(label=upper_or_lower)
-                col2.pyplot(plt)
-                plt.savefig("openmc_plot_weightwindow_image.png")
-
-                with open("openmc_plot_weightwindow_image.png", "rb") as file:
-                    col1.download_button(
-                        label="Download image",
-                        data=file,
-                        file_name="openmc_plot_weightwindow_image.png",
-                        mime="image/png"
+                if axis_to_slice == "X":
+                    tally_aligned = reshaped_tally.transpose(1, 2, 0)
+                    bb_index = 0
+                    x_label = "Y [cm]"
+                    y_label = "Z [cm]"
+                elif axis_to_slice == "Y":
+                    tally_aligned = reshaped_tally.transpose(
+                        0, 1, 2
                     )
+                    bb_index = 1
+                    x_label = "X [cm]"
+                    y_label = "Z [cm]"
+                else:  # axis_to_slice == 'Z':
+                    tally_aligned = reshaped_tally.transpose(2, 0, 1)
+                    bb_index = 2
+                    x_label = "X [cm]"
+                    y_label = "Y [cm]"
+
+                left = mesh.lower_left[bb_index]
+                right = mesh.upper_right[bb_index]
+                bottom = mesh.lower_left[bb_index]
+                top = mesh.upper_right[bb_index]
+
+                slice_value = col1.slider(
+                    label="slice index",
+                    min_value=0,
+                    max_value=len(tally_aligned) - 1,
+                    value=int(len(tally_aligned) / 2),
+                )
+
+                log_lin_scale = col1.radio(
+                    "Normalization", options=["log", "linear"],
+                    key='ww_log_lin_scale')
+                if log_lin_scale == "linear":
+                    norm = None
+                else:
+                    norm = LogNorm()
+
+                image_slice = tally_aligned[slice_value]
+
+                if axis_to_slice == "Y":
+                    image_slice = np.rot90(image_slice)
+                if axis_to_slice == "Z":
+                    image_slice = np.rot90(image_slice)
+                if axis_to_slice == "X":
+                    image_slice = np.flipud(image_slice)
+
+                plt.cla()
+                plt.clf()
+
+                plt.axes(title="Tally value", xlabel=x_label, ylabel=y_label)
+                # could be assigned like this
+                # plt.xlabel(x_label)
+                # plt.ylabel(y_label)
+                # plt.title('Tally value')
+                if np.amax(image_slice) == np.amin(image_slice) and norm is not None:
+                    msg = "slice contains the uniform values, can't be plotted on log scale"
+                    format = f'<p style="font-family:sans-serif; color:Red; font-size: 30px;">{msg}</p>'
+                    col2.markdown(format, unsafe_allow_html=True)
+                else:
+                    plt.imshow(
+                        X=image_slice,
+                        extent=(left, right, bottom, top),
+                        norm=norm
+                    )
+                    plt.colorbar(label=upper_or_lower)
+                    col2.pyplot(plt)
+                    plt.savefig("openmc_plot_weightwindow_image.png")
+
+                    with open("openmc_plot_weightwindow_image.png", "rb") as file:
+                        col1.download_button(
+                            label="Download image",
+                            data=file,
+                            file_name="openmc_plot_weightwindow_image.png",
+                            mime="image/png"
+                        )
