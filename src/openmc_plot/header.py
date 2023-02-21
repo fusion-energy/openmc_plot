8ee3f5fdeb7f67ce56a21ff41edad8c1a269b001
shimwell <mail@jshimwell.com>
2023-02-21 19:35:01 +0000

    version bump

--- src/openmc_plot/header.py
+++ src/openmc_plot/header.py
@@ -22,7 +22,7 @@ def header():
 
     st.write(
         """
-            # OpenMC plot ```v0.2.0```
+            # OpenMC plot ```v0.2.1```
 
             ### ⚛ A geometry plotting user interface for OpenMC.
 
